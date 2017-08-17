bfc2b1a9f42697a85fa97c62d75583a541c68066
Markus Frosch <markus.frosch@icinga.com>
2017-08-17 17:33:50 +0200

    Add copy button to interface

--- public/js/module.js
+++ public/js/module.js
@@ -9,6 +9,8 @@
 
     EventDB.prototype = {
         initialize: function() {
+            this.module.on('rendered', this.enableCopyable);
+
             var addCSSRule = function(sheet, selector, rules, index) {
                 if('insertRule' in sheet) {
                     sheet.insertRule(selector + '{' + rules + '}', index);
@@ -33,6 +35,54 @@
                 'width: 50%',
                 0
             );
+        },
+        enableCopyable: function() {
+            var e = this;
+            $('.copyable').each(function() {
+                var $button = $('<a>')
+                    .attr('href', '#')
+                    .addClass('action-link icon icon-globe copyable-button')
+                    .text('Copy text');
+
+                $button.on('click', function() {
+                    var $el = $(this).parent().siblings('.copyable');
+                    if ($el) {
+                        e.selectText($el[0]);
+                        document.execCommand('copy');
+                        setTimeout(function () {
+                            e.clearSelection()
+                        }, 500);
+                        if (icinga) {
+                            icinga.loader.createNotice('info', 'Text copied to clipboard')
+                        }
+                    }
+                });
+
+                var $div = $('<div>').addClass('copyable-actions').append($button);
+
+                $(this).before($div);
+            });
+        },
+        selectText: function (text) {
+            var doc = document, range, selection;
+            if (doc.body.createTextRange) {
+                range = document.body.createTextRange();
+                range.moveToElementText(text);
+                range.select();
+            } else if (window.getSelection) {
+                selection = window.getSelection();
+                range = document.createRange();
+                range.selectNodeContents(text);
+                selection.removeAllRanges();
+                selection.addRange(range);
+            }
+        },
+        clearSelection: function() {
+            if (document.selection) {
+                document.selection.empty();
+            } else if (window.getSelection) {
+                window.getSelection().removeAllRanges();
+            }
         }
     };
 
